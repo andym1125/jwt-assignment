f39fcdfe7bf4b3a64da02cdd7516ba73932eaef6
Andy <37462713+andym1125@users.noreply.github.com>
2024-02-05 19:16:09 -0600

    Implemented kid support and fixed jwks endpoint

--- server.ts
+++ server.ts
@@ -1,22 +1,35 @@
-import express, { Application } from 'express';
+import express, { Application, Request } from 'express';
 import * as jose from 'jose'
+import { JSONWebKeySet, KeyLike } from 'jose';
 const app: Application = express();
 
-const {publicKey, privateKey} = await jose.generateKeyPair('PS256')
-console.log('Public Key: ' + (publicKey))
+let publics: JSONWebKeySet = { keys: [] };
+let privates: KeyLike[] = [];
 
 app.get('/', async (req, res) => {
-	res.send(await jose.exportSPKI(publicKey))
+	console.log(`Request for JWKS\n${publics}\n`)
+	res.status(200).send(JSON.stringify(publics))
 })
 
-app.post('/auth', async (req, res) => {
+app.post('/auth', async (req : Request<{expired?: string}>, res) => {
+	const expiration = 60 * 60 * (req.params.expired ? -1 : 1) + (Date.now() / 1000)
+	const kid = Date.now().toString()
+	const {publicKey, privateKey} = await jose.generateKeyPair('RS256')
+	if(!req.params.expired) {
+		let jwk = await jose.exportJWK(publicKey)
+		jwk.kid = kid
+		publics.keys.push(jwk)
+		privates.push(privateKey)
+	}
+
 	const jwt = await new jose.SignJWT({})
-		.setProtectedHeader({alg: 'PS256'})
+		.setProtectedHeader({alg: 'RS256', kid: kid})
 		.setIssuedAt()
-		.setExpirationTime('2h')
+		.setExpirationTime( expiration)
 		.sign(privateKey)
-	console.log('Issued: ' + jwt)
-	res.send(jwt)
+
+	console.log('Issued ' + (req.params.expired ? 'expired' : 'valid') + ' token')
+	res.status(200).send(jwt)
 });
 
 app.listen(8080, () => {
